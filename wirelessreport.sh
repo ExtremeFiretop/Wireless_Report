d04e44a2ff07224323c0e317a1b81c142dc0ea3e
JB1366 <jb8745@proton.me>
2026-08-04 08:44:06 -0500

    Wireless Report

--- wirelessreport.sh
+++ wirelessreport.sh
@@ -26,7 +26,7 @@
 #        shellcheck shell=sh disable=SC2086,SC2155,SC3043         #
 #=================================================================#
 
-SCRIPT_VERSION="2.0.8"
+SCRIPT_VERSION="2.0.9"
 INSTALL_DIR="/jffs/addons/wireless_report"
 REPORT_SCRIPT="$INSTALL_DIR/wirelessreport.sh"
 SYSTEM_MENU="/www/require/modules/menuTree.js"
